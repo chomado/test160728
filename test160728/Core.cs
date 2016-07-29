cd98323464b548b5030a2416ba355d0f8e8ae455
chomado <chomado@chomabook-2.local>
2016-07-29 11:05:08 -0700

    rename to "Core"

---
+++ test160728/Core.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Core
+{
+    /// <summary>
+    /// 投稿に使う各種キーの集まり。（型定義）
+    /// </summary>
+    public class Keys
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessSecret { get; set; }
+    }
+
+}
